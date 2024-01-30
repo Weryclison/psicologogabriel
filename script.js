18be757233f03f88bc3b5405695766ddbc1c4c86
Weryclison Nandi <weryclison.nandi@gmail.com>
2024-01-30 13:25:40 -0300

    first commit

---
+++ script.js
@@ -0,0 +1,7 @@
+import menuMobile from "./js/menu-mobile.js";
+import scrollSuave from "./js/scroll-suave.js";
+import perguntas from "./js/abrir-resposta.js";
+
+menuMobile();
+scrollSuave();
+perguntas();
